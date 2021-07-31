04b216ca59d95a2f0ac69befb0067a27bf7ebd24
shubham <gshubham030@gmail.com>
2021-07-31 11:01:10 +0530

    firebase added

---
+++ lib/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TrelloCloneApp extends StatelessWidget {
+  const TrelloCloneApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
